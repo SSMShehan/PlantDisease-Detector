bf3a447e374173953a0895fae8ab65b9df60372d
SSMShehan <shehanmadusanka775@gmail.com>
2026-09-19 11:56:58 +0530

    chore: fix test compilation error and setup for Android Studio

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@ void main() {
   testWidgets('App launches and shows splash screen',
       (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const AgroLuxApp());
+    await tester.pumpWidget(const CropGuardApp());
     // Splash screen should be visible initially
     expect(find.text('PlantDoc – Crop Disease Detector'), findsNothing);
     await tester.pump(const Duration(milliseconds: 100));
